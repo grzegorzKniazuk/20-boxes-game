1a751d46f29abaa260f23ffd98d1d3a5da4a37fa
= <grzegorz.kniazuk@gmail.com>
2019-02-18 20:39:26 +0100

    17.02 - end summary component

--- src/app/shared/components/end-game-summary/end-game-summary.component.ts
+++ src/app/shared/components/end-game-summary/end-game-summary.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { GameStateService } from '../../../core/services/game-state.service';
-import { Statistics } from '../../../core/interfaces/statistics';
+import { GameStateService } from 'src/app/core/services/game-state.service';
+import { Statistics } from 'src/app/core/interfaces/statistics';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
@@ -13,7 +13,7 @@ export class EndGameSummaryComponent implements OnInit {
 
   public summaryStatistics: Statistics;
   public readonly winnerURL = '../../../../assets/images/winner.svg';
-  public readonly beatenURL = '../../../../assets/images/beaten.png';
+  public readonly beatenURL = '../../../../assets/images/beaten.svg';
 
   constructor(private gameStateService: GameStateService,
               @Inject(MAT_DIALOG_DATA) private isWinner: boolean,
